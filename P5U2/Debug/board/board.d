8c319780d127c87225a10a90b5cdb00099919bcb
Fernando Julián <juliaragonm@hotmail.com>
2022-03-07 23:50:24 -0600

    PWM UART Practice
    Buzzer working and choice for songs (only 1 song)

---
+++ P5U2/Debug/board/board.d
@@ -0,0 +1,30 @@
+board/board.o board/board.d: ../board/board.c ../board/board.h \
+ ../board/clock_config.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_common.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/fsl_device_registers.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/MKL25Z4.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/core_cm0plus.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/core_cmInstr.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/cmsis_gcc.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/core_cmFunc.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/system_MKL25Z4.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/MKL25Z4_features.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_clock.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_common.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_gpio.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\utilities/fsl_debug_console.h
+../board/board.h:
+../board/clock_config.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_common.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/fsl_device_registers.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/MKL25Z4.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/core_cm0plus.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/core_cmInstr.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/cmsis_gcc.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/core_cmFunc.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/system_MKL25Z4.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/MKL25Z4_features.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_clock.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_common.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_gpio.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\utilities/fsl_debug_console.h:
---
+++ P5U2/Debug/drivers/PITDrv.d
@@ -0,0 +1,31 @@
+drivers/PITDrv.o drivers/PITDrv.d: ../drivers/PITDrv.c \
+ ../drivers/PITDrv.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\board/board.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\board/clock_config.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_common.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/fsl_device_registers.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/MKL25Z4.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/core_cm0plus.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/core_cmInstr.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/cmsis_gcc.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/core_cmFunc.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/system_MKL25Z4.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/MKL25Z4_features.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_clock.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_common.h \
+ C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_gpio.h
+../drivers/PITDrv.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\board/board.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\board/clock_config.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_common.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/fsl_device_registers.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/MKL25Z4.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/core_cm0plus.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/core_cmInstr.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/cmsis_gcc.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/core_cmFunc.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/system_MKL25Z4.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\CMSIS/MKL25Z4_features.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_clock.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_common.h:
+C\:\Users\lfara\Documents\MCUXpressoIDE_11.4.1_6260\workspace\PWMUART_TEST\drivers/fsl_gpio.h:
